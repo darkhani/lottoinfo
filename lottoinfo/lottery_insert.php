01b724903f8c91221e5ea83650b212e6f26f3c5f
dandyhani <darkhani@icloud.com>
2024-06-30 12:44:55 +0900

    1. 어드민에서 성공시 얼럿을 추가 합니다.

--- lottoinfo/lottery_insert.php
+++ lottoinfo/lottery_insert.php
@@ -33,8 +33,11 @@ $sql = "INSERT INTO lottery (round, fdate,fmdate, winner1, amount1, winner2, amo
 //INSERT INTO lottery (round, fdate, winner1, amount1, winner2, amount2, winner3, amount3, winner4, amount4, winner5, amount5, number1, number2, number3, number4, number5, number6, bonus) VALUES ('1', '2002-12-07', '0 ', '0 ', '1 ', '143934100 ', '28 ', '5140500 ', '2537 ', '113400 ', '40155 ', '10000 ', '10', '23', '29', '33', '37', '40', '16');
 
 if ($conn->query($sql) === TRUE) {
-    header("Location: lottoinfo_admin.html");
-    echo "데이터가 성공적으로 입력되었습니다.";
+    // 데이터 삽입 성공 시 알림 창을 표시하고 관리자 페이지로 리다이렉션
+    echo "<script>
+            alert('데이터가 정상적으로 추가 되었습니다.');
+            window.location.href = 'lottoinfo_admin.html';
+          </script>";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
